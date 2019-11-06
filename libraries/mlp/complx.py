Fix norm calculation to work with Float numbers

libraries/mlp/complx.py
@@ -3,12 +3,14 @@ import numpy as np
 
 
 def to_polar(complx):
-    magnitude = torch.norm(complx, dim=0)
+    
+    magnitude = (complx[0] ** 2 + complx[1] ** 2)**0.5
+    # magnitude = torch.norm(complx, dim=0)
     phase = torch.atan(complx[1] / complx[0])
     
     phase[complx[0] < 0] = phase[complx[0] < 0] + np.pi
     phase = phase - np.pi/2
-    
+
     return magnitude, phase
     
     

libraries/mlp/dataset.py
@@ -103,7 +103,7 @@ class WAVAudioDS(PatchedStrokeDS):
         
     @staticmethod
     def freqs_to_torch(freqs, max_freqs):
-        return torch.from_numpy(freqs.view(np.float32).reshape(129, 2, -1)[:max_freqs].transpose(1, 0, 2))
+        return torch.from_numpy(freqs.reshape(129, 2, -1)[:max_freqs].transpose(1, 0, 2).astype(np.float32))
         
     @staticmethod
     def torch_to_freqs(audio_freqs, denorm=lambda x:x):